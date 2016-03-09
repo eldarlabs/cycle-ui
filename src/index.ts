9145a2c64667cfea6c1bcc3bdd35774ec99883b1
arlair <arlair@yahoo.com>
2016-03-10 07:45:44 +0800

    remove typings absolute path for type files that made it in by mistake, typings workaround for index.ts using /index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-export { Input } from './input';
-export { RadioButton } from './radio';
-export { RadioGroup } from './radio';
+// export { Input } from './input';
+// export { RadioButton } from './radio';
+// export { RadioGroup } from './radio';
+export { Input } from './input/index';
+export { RadioButton } from './radio/index';
+export { RadioGroup } from './radio/index';
