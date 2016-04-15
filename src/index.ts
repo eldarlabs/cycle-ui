17da8356f57e3a47305b31cf735ebb2dbc8818c0
arlair <arlair@yahoo.com>
2016-04-15 12:08:55 +0800

    fix(components): App component wrapper is no longer needed

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-export { App } from './app/index';
 export { Button, ButtonFactory } from './button/index';
 export { Card, CardTitle, CardText, CardActions } from './card/index';
 export { Dialog } from './dialog/index';
